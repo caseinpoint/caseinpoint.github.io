c9df448cc49097d517da22a90a6a3485489ba93d
caseinpoint <drues.other.email@gmail.com>
2023-06-02 12:42:21 -0700

    successfully displaying 1 image!

--- static/js/twentytwentythree.js
+++ static/js/twentytwentythree.js
@@ -6,6 +6,19 @@ const ACCESS_TOKEN = checkOAuth();
 // "Selfies" album id
 const ALBUM_ID = 'AFcmz6DlGl6r_sdivkYO7ahqZsmBSAeBHvTe1UuYEq7STWLsYor-Pun9ZMzmHMSY_526lQm6gsMH'
 
+// list albums once to find "Selfies" album id:
+/*
+fetch('https://photoslibrary.googleapis.com/v1/albums', {
+	method: 'get',
+	headers: {
+		'Content-Type': 'application/json',
+		'Authorization': 'Bearer ' + accessToken,
+	},
+})
+	.then((res) => res.json())
+	.then((data) => console.log(data))
+*/
+
 
 function checkOAuth() {
 	// if access_token is missing or expired redirect to login page
@@ -25,20 +38,6 @@ function checkOAuth() {
 
 	return accessToken;
 }
-
-
-// list albums once to find "Selfies" album id
-/*
-fetch('https://photoslibrary.googleapis.com/v1/albums', {
-	method: 'get',
-	headers: {
-		'Content-Type': 'application/json',
-		'Authorization': 'Bearer ' + accessToken,
-	},
-})
-	.then((res) => res.json())
-	.then((data) => console.log(data))
-*/
 
 
 async function getAlbumPhotosPage(accessToken, albumId, pageToken=null) {
@@ -66,26 +65,63 @@ async function getAlbumPhotosPage(accessToken, albumId, pageToken=null) {
 async function getAllAlbumPhotos(accessToken, albumId) {
 	let allPhotos = [];
 	
-	let albumResults = await getAlbumPhotosPage(ACCESS_TOKEN, ALBUM_ID);
+	let albumResults = await getAlbumPhotosPage(accessToken, albumId);
 	
 	let mediaItems = albumResults.mediaItems;
 	allPhotos = allPhotos.concat(mediaItems);
 	
 	let nextPageToken = albumResults.nextPageToken;
 	while (nextPageToken) {
-		let nextResults = await getAlbumPhotosPage(ACCESS_TOKEN, ALBUM_ID, nextPageToken);
+		albumResults = await getAlbumPhotosPage(accessToken, albumId, nextPageToken);
 
-		mediaItems = nextResults.mediaItems;
+		mediaItems = albumResults.mediaItems;
 		allPhotos = allPhotos.concat(mediaItems);
 	
-		nextPageToken = nextResults.nextPageToken;
+		nextPageToken = albumResults.nextPageToken;
 	}
 	
 	return allPhotos;
 }
 
 
+function initImgs(photosArr) {
+	const urlCurrent = photosArr[0].baseUrl;
+	const widthCurrent = photosArr[0].mediaMetadata.width;
+	const heightCurrent = photosArr[0].mediaMetadata.height;
+
+	const imgCurrent = document.createElement('img');
+	imgCurrent.classList.add('img-fluid');
+	imgCurrent.id = 'img_current';
+	imgCurrent.src = `${urlCurrent}=w${widthCurrent}-h${heightCurrent}`;
+	
+
+	const carousel = document.getElementById('carousel');
+	carousel.appendChild(imgCurrent);
+}
+
+
+function compareRandom(a, b) {
+	return 0.5 - Math.random();
+}
+
+function comparePhotoDate(a, b) {
+	const aDate = new Date(a.mediaMetadata.creationTime);
+	const bDate = new Date(b.mediaMetadata.creationTime);
+	return aDate - bDate;
+}
+
+function sortPhotos(photosArr, method) {
+	if (method === 'date') {
+		photosArr.sort(comparePhotoDate)
+	} else if (method === 'random') {
+		photosArr.sort(compareRandom);
+	}
+}
+
+
 (async function main() {
 	const allPhotos = await getAllAlbumPhotos(ACCESS_TOKEN, ALBUM_ID);
 	console.log(allPhotos);
+	
+	initImgs(allPhotos);
 })()
