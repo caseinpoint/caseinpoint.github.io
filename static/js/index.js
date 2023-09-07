b3af0dd3c1d028a9ad3c7d4db2811d7c4a287158
caseinpoint <drues.other.email@gmail.com>
2023-09-06 17:53:08 -0700

    improved color behavior and contrast

--- static/js/index.js
+++ static/js/index.js
@@ -1,3 +1,7 @@
+let centerX;
+let centerY;
+let minAxis;
+
 let bgrHue = 176; // background hue: 0-360
 let fgrHue = (bgrHue + 180) % 360; // foreground hue: 0-360
 let bgrSat = 83; // background saturation: 0-100
@@ -16,18 +20,18 @@ function degToRad(deg) {
 }
 
 
-function getAngle(deltaX, deltaY) {
-	if (deltaX === 0) {
-		if (deltaY > 0) return 90;
-		else if (deltaY < 0) return 270;
+function getAngle(adjacent, opposite) {
+	if (adjacent === 0) {
+		if (opposite > 0) return 90;
+		else if (opposite < 0) return 270;
 		else return 0;
 	}
 
-	const rad = Math.atan(deltaY / deltaX);
+	const rad = Math.atan(opposite / adjacent);
 	const deg = radToDeg(rad);
 
-	if (deltaX > 0) {
-		if (deltaY >= 0) return deg;
+	if (adjacent > 0) {
+		if (opposite >= 0) return deg;
 		else return 360 + deg;
 	} else {
 		return 180 + deg;
@@ -72,9 +76,6 @@ function setFontColor() {
 
 
 function setHSB() {
-	const centerX = Math.trunc(width / 2);
-	const centerY = Math.trunc(height / 2);
-	const medAxis = Math.trunc((centerX + centerY) / 2);
 	const x = mouseX - centerX;
 	const y = centerY - mouseY;
 
@@ -83,25 +84,34 @@ function setHSB() {
 	fgrHue = (bgrHue + 180) % 360;
 
 	const hypotenuse = getHypotenuse(x, y);
-	const hypPercent = Math.trunc(hypotenuse / medAxis * 100);
-	bgrSat = constrain(hypPercent, 50, 100);
-	fgrSat = 100 - bgrSat;
-	bgrBright = constrain(hypPercent, 30, 80);
-	fgrBright = 100 - bgrBright;
+	const hypPercent = constrain(Math.trunc(hypotenuse / minAxis * 100), 0, 100);
+	bgrSat = Math.round(map(hypPercent, 0, 100, 40, 100));
+	fgrSat = 100 - hypPercent;
+	bgrBright = Math.round(map(hypPercent, 0, 100, 40, 80));
+	fgrBright = 100 - hypPercent;
 }
 
 
 function setup() {
 	createCanvas(windowWidth, windowHeight);
+	centerX = Math.trunc(width / 2);
+	centerY = Math.trunc(height / 2);
+	minAxis = centerX <= centerY ? centerX : centerY;
+
 	colorMode(HSB);
 	background(bgrHue, bgrSat, bgrBright);
+
 	setFontColor();
+
 	noLoop();
 }
 
 
 function windowResized() {
 	resizeCanvas(windowWidth, windowHeight);
+	centerX = Math.trunc(width / 2);
+	centerY = Math.trunc(height / 2);
+	minAxis = centerX <= centerY ? centerX : centerY;
 }
 
 
