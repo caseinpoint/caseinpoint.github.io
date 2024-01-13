70b13f0db0e76adf9af944e4f37af542273d2359
caseinpoint <drues.other.email@gmail.com>
2024-01-13 14:21:15 -0700

    add levelProgression.json

--- static/js/chaosMage.jsx
+++ static/js/chaosMage.jsx
@@ -1,17 +1,34 @@
+function getCategoryArray() {
+	/* Get a new, shuffled array of spell categories */
+
+	const categories = ['atk', 'atk', 'def', 'def', 'icn', 'icn'];
+
+	for (let n = 0; n < 3; n++) {
+		for (let i in categories) {
+			const r = Math.floor(Math.random() * categories.length);
+			[categories[i], categories[r]] = [categories[r], categories[i]];
+		}
+	}
+
+	return categories;
+}
+
 function SpellsTracker(props) {
-	/* Track daily and per battle spells based on character level */
+	/* React compmponent: Track daily/per battle spells for character level */
 
 	// useEffect to load most recent values from localStorage
-	// long rest button to reset
+	// full heal-up button to reset
 }
 
 function OptionsMenu(props) {
-	/* Select character talents and feats */
+	/* React component: Select character talents and feats */
 
 	// useEffect to load previous selections form localStorage
 }
 
 function Root(props) {
+	/* Root React component */
+
 	return (
 		<div className="container-fluid">
 			<h1>Testing</h1>
