b20278f9b572b9894667e23a31766da9c32c5b7a
caseinpoint <drues.other.email@gmail.com>
2025-01-16 13:44:59 -0700

    scroll down basics completed

--- static/js/indexText.js
+++ static/js/indexText.js
@@ -1,7 +1,10 @@
 const MAIN_ELEMENT = document.getElementById('main_content');
 const ELEMENTS = {
+	divs: [],
 	divIdx: 0,
-	divs: []
+	yScroll: 100,
+	elIdx: 0,
+	xScroll: 0
 };
 
 
@@ -14,10 +17,7 @@ function initText(data) {
 		// create object to track elements and scroll position
 		const divObj = {
 			div: null,
-			divScroll: 100,
-			elIdx: 0,
 			els: [],
-			elScroll: 0
 		};
 		ELEMENTS.divs.push(divObj);
 
@@ -63,6 +63,7 @@ function initText(data) {
 			const deployedP = document.createElement('p');
 			const deployedA = document.createElement('a');
 			deployedA.href = d.link;
+			deployedA.target = '_blank';
 			const dIcon = document.createElement('i');
 			dIcon.classList.add('bi');
 			deployedA.insertAdjacentElement('afterbegin', dIcon);
@@ -82,6 +83,7 @@ function initText(data) {
 			const githubP = document.createElement('p');
 			const githubA = document.createElement('a');
 			githubA.href = d.github;
+			githubA.target = '_blank';
 			const gIcon = document.createElement('i');
 			gIcon.classList.add('bi', 'bi-github');
 			githubA.insertAdjacentElement('afterbegin', gIcon);
@@ -91,13 +93,12 @@ function initText(data) {
 			divObj.els.push(githubP);
 		}
 
-		// set first div to display, others hidden
 		if (i === 0) {
-			// set up scrolling of next object
-			divObj.elIdx = divObj.els.length;
-			divObj.elScroll = 100;
+			// set first elIdx to indicate all elements scrolled in
+			ELEMENTS.elIdx = divObj.els.length - 1;
 		} else {
-			div.classList.add('d_none');
+			// set second through last elements to hidden
+			// div.classList.add('d_none');
 			for (let e of divObj.els) {
 				e.classList.add('hide_right');
 			}
@@ -107,20 +108,75 @@ function initText(data) {
 
 
 function onScroll(evt) {
-	// scroll elements out of and view
+	// scroll elements out of and into view
 
-	evt.preventDefault();
+	// evt.deltaY > 0 means wheel down
+	// 100 % incr === 0
+	const incr = evt.deltaY > 0 ? -5 : 5;
 
-	const incr = evt.deltaY > 0 ? -1 : 1;
+	const divObj = ELEMENTS.divs[ELEMENTS.divIdx];
+
+	// check if scrolling down
+	if (incr < 0) {
+
+		// check if all sub-elements are scrolled into view
+		if (ELEMENTS.elIdx === divObj.els.length - 1 && ELEMENTS.xScroll === 0) {
+
+			// check if on last div
+			if (ELEMENTS.divIdx === ELEMENTS.divs.length - 1) {
+				// can't scroll down any farther
+				return;
+			}
+
+			const newY = ELEMENTS.yScroll + incr;
+
+
+			if (newY >= Math.abs(incr)) {
+				ELEMENTS.yScroll = newY;
+				divObj.div.style.height = `${newY}%`
+			} else {
+				divObj.div.classList.add('d_none');
+
+				ELEMENTS.divIdx++;
+				ELEMENTS.yScroll = 100;
+				ELEMENTS.elIdx = 0;
+				ELEMENTS.xScroll = 100;
+
+				const nextDivObj = ELEMENTS.divs[ELEMENTS.divIdx];
+				nextDivObj.div.classList.remove('d_none');
+			}
+		}
+		// else not all sub-elements are scrolled in
+		else {
+			const newX = ELEMENTS.xScroll + incr;
+			if (newX >= 0) {
+				ELEMENTS.xScroll = newX;
+				const el = divObj.els[ELEMENTS.elIdx];
+				el.style.marginLeft = `${newX}vw`;
+			} else {
+				ELEMENTS.xScroll = 100;
+				ELEMENTS.elIdx++;
+			}
+		}
+	}
+	// else scrolling up
+	else {
+		// check if on first div and div fully scrolled into view
+		if (ELEMENTS.divIdx === 0 && ELEMENTS.yScroll === 100) {
+			// can't scroll up any farther
+			return;
+		}
+	}
 }
 
 
 (function main() {
+	// TODO: add browser check and alert for wheel event
+	MAIN_ELEMENT.addEventListener('wheel', onScroll, { passive: true });
+
 	// get text JSON from /static
 	fetch('/static/json/index.json')
 		.then((response) => response.json())
 		.then((data) => { initText(data); })
 		.catch((error) => { console.error('Error:', error) });
-
-	MAIN_ELEMENT.addEventListener('wheel', onScroll);
 })();
