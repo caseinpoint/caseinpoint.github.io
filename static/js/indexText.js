feeb7a35f0392f017dd7877541af17053cea9289
caseinpoint <drues.other.email@gmail.com>
2025-05-06 16:16:18 -0600

    completed scrolling up!

--- static/js/indexText.js
+++ static/js/indexText.js
@@ -23,6 +23,7 @@ function initText(data) {
 
 		// create elements
 		const div = document.createElement('div');
+		div.id = `div_${i}`;
 		div.classList.add('data_div');
 		MAIN_ELEMENT.appendChild(div);
 		divObj.div = div;
@@ -40,6 +41,7 @@ function initText(data) {
 		if (d.img != null) {
 			const img = document.createElement('img');
 			img.src = d.img;
+			img.alt = d.h2;
 			div.appendChild(img);
 			divObj.els.push(img);
 		}
@@ -98,7 +100,6 @@ function initText(data) {
 			ELEMENTS.elIdx = divObj.els.length - 1;
 		} else {
 			// set second through last elements to hidden
-			// div.classList.add('d_none');
 			for (let e of divObj.els) {
 				e.classList.add('hide_right');
 			}
@@ -112,7 +113,7 @@ function onScroll(evt) {
 
 	// evt.deltaY > 0 means wheel down
 	// 100 % incr === 0
-	const incr = evt.deltaY > 0 ? -10 : 10;
+	const incr = evt.deltaY > 0 ? -12.5 : 12.5;
 
 	const divObj = ELEMENTS.divs[ELEMENTS.divIdx];
 
@@ -133,7 +134,7 @@ function onScroll(evt) {
 
 			if (newY >= 0) {
 				ELEMENTS.yScroll = newY;
-				divObj.div.style.height = `${newY}%`
+				divObj.div.style.height = `${newY}%`;
 			} else {
 				divObj.div.classList.add('d_none');
 
@@ -145,9 +146,7 @@ function onScroll(evt) {
 				const nextDivObj = ELEMENTS.divs[ELEMENTS.divIdx];
 				nextDivObj.div.classList.remove('d_none');
 			}
-		}
-		// else not all sub-elements are scrolled in
-		else {
+		} else { // else not all sub-elements are scrolled in
 			// scroll across
 			const newX = ELEMENTS.xScroll + incr;
 
@@ -160,21 +159,84 @@ function onScroll(evt) {
 				ELEMENTS.elIdx++;
 			}
 		}
-	}
-	// else scrolling up
-	else {
-		// check if on first div and div fully scrolled into view
+	} else { // else scrolling up
+		// check if on first div and div is at 100% height
 		if (ELEMENTS.divIdx === 0 && ELEMENTS.yScroll === 100) {
 			// can't scroll up any farther
 			return;
 		}
+
+		// check if div height < 100% or all sub-elements are scrolled out of view
+		if (ELEMENTS.yScroll < 100 || ELEMENTS.elIdx === 0 && ELEMENTS.xScroll === 100) {
+			// scroll up
+			const newY = ELEMENTS.yScroll + incr;
+
+			if (newY <= 100) {
+				ELEMENTS.yScroll = newY;
+				divObj.div.style.height = `${newY}%`;
+			} else {
+				ELEMENTS.yScroll = 0;
+				ELEMENTS.xScroll = 0;
+				
+				ELEMENTS.divIdx--;
+				const prevDivObj = ELEMENTS.divs[ELEMENTS.divIdx];
+				ELEMENTS.elIdx = prevDivObj.els.length - 1;
+				prevDivObj.div.classList.remove('d_none');
+			}
+		} else { // else not all sub-elements are scrolled out
+			// scroll back
+			const newX = ELEMENTS.xScroll + incr;
+
+			if (newX <= 100) {
+				ELEMENTS.xScroll = newX;
+				const el = divObj.els[ELEMENTS.elIdx];
+				el.style.marginLeft = `${newX}vw`;
+			} else {
+				ELEMENTS.xScroll = 0;
+				ELEMENTS.elIdx--;
+			}
+		}
+
+	}
+}
+
+// // scroll back
+// const newX = ELEMENTS.xScroll + incr;
+
+// if (newX <= 100) {
+// 	ELEMENTS.xScroll = newX;
+// 	const el = divObj.els[ELEMENTS.elIdx];
+// 	el.style.marginLeft = `${newX}vw`;
+// } else {
+// 	ELEMENTS.xScroll = 0;
+// 	ELEMENTS.elIdx--;
+// }
+
+let touchY = 0;
+
+function onTouchMove(evt) {
+	// determine if scrolling on touchscreen and pass on to onScroll
+
+	// only one-finger scroll
+	if (evt.changedTouches.length === 1) {
+		const y = evt.changedTouches[0].clientY;
+
+		if (y < touchY) {
+			// scroll down
+			onScroll({ deltaY: 1});
+		} else {
+			// scroll up
+			onScroll({ deltaY: -1 });
+		}
+
+		touchY = y;
 	}
 }
 
 
 (function main() {
-	// TODO: add browser check and alert for wheel event
 	MAIN_ELEMENT.addEventListener('wheel', onScroll, { passive: true });
+	MAIN_ELEMENT.addEventListener('touchmove', onTouchMove);
 
 	// get text JSON from /static
 	fetch('/static/json/index.json')
